e02efe608443ee40e56ce18db8a8aac9d0a9907c
Ricardo Fernández <ricardo.a.fernandez.a@gmail.com>
2025-03-05 18:57:07 -0600

    add customers table

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,11 @@
-export default function Page() {
-  return <p>Customers Page</p>;
+import { fetchFilteredCustomers } from "@/app/lib/data";
+import CustomersTable from "@/app/ui/customers/table";
+
+export default async function Page(props: {
+  searchParams?: Promise<{ query?: string }>;
+}) {
+  const params = await props.searchParams;
+  const customers = await fetchFilteredCustomers(params?.query ?? "");
+
+  return <CustomersTable customers={customers} />;
 }
