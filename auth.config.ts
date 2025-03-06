6dd84f9e14753581fbc7ae81042b6530df12fc97
Ricardo Fernández <ricardo.a.fernandez.a@gmail.com>
2025-03-05 18:26:54 -0600

    add auth

---
+++ auth.config.ts
@@ -0,0 +1,26 @@
+import type { NextAuthConfig } from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import postgres from "postgres";
+import { z } from "zod";
+import { User } from "@/app/lib/definitions";
+import bcrypt from "bcrypt";
+
+export const authConfig = {
+  pages: {
+    signIn: "/login",
+  },
+  callbacks: {
+    authorized({ auth, request: { nextUrl } }) {
+      const isLoggedIn = !!auth?.user;
+      const isOnDashboard = nextUrl.pathname.startsWith("/dashboard");
+      if (isOnDashboard) {
+        if (isLoggedIn) return true;
+        return false;
+      } else if (isLoggedIn) {
+        return Response.redirect(new URL("/dashboard", nextUrl));
+      }
+      return true;
+    },
+  },
+  providers: [],
+} satisfies NextAuthConfig;
